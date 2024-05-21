3dedda1bbcc02e3b47a4b09d69204b33db8f3766
Davi Camilo Caetano <davi357c@gmail.com>
2024-05-21 16:00:20 -0300

    Novo arquivo

---
+++ Yeah.py
@@ -0,0 +1,7 @@
+question = 0
+while question not in ["yes", "no"]:
+    question = input('Are you Grrr!? Yes or no?').lower()
+    if question == "yes":
+        print("WOW! THAT'S SO MUCH POGGERS!")
+    else:
+        print("That's not poggers D:")
